909bbc28ba76e364206b8cd4cd6ae2aaeb0f5ab2
Nathan Bills <nathan@nerdmeeting.com>
2024-12-26 18:37:16 -0700

    Use an aws_ami data source to load the correct AMI ID for the current region.

--- main.tf
+++ main.tf
@@ -71,3 +71,13 @@ data "terraform_remote_state" "vpc" {
     }
   }
 }
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners = ["amazon"]
+  
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
